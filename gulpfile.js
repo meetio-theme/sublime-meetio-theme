c4bea63b04836e39c813bdbf4e57eff2855ab36a
Mauro Vieira <mauro.vieira@namecheap.com>
2018-12-13 19:04:25 +0000

    feat: updated name of options.

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ const array = [];
 let object = {};
 
 gulp.task('build', () => {
-    fs.readdirSync('./modules').forEach(file => {
-        file = path.join(__dirname, '/modules/' + file);
-        fs.readFile(file, 'utf8', function (err, data) {
+    fs.readdirSync('./templates').forEach(file => {
+        file = path.join(__dirname, '/templates/' + file);
+        fs.readFile(file, 'utf8', (err, data) => {
             if (err) throw err;
             object = JSON.parse(data);
-            Object.keys(object).map(function(objectKey, index) {
+            Object.keys(object).map((objectKey) => {
                 var value = object[objectKey];
                 array.push(value);
             });
             const json = JSON.stringify(array, null, 4);
-            fs.writeFile("Material-Lite.sublime-theme", json, function(err) {
+            fs.writeFile("Material-Lite.sublime-theme", json, (err) => {
                 if (err) console.log(err);
                 console.log("The file was saved!");
             });
         });
-    })
+    });
 });
