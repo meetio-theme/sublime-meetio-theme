04f34b3ca37df0e2af827f091b5e79f53a5308db
Mauro Vieira <mauro.vieira@namecheap.com>
2018-12-14 12:36:00 +0000

    fix: updated gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,9 @@ const array = [];
 let object = {};
 
 gulp.task('build', () => {
-<<<<<<< HEAD
     fs.readdirSync('./templates').forEach(file => {
         file = path.join(__dirname, '/templates/' + file);
         fs.readFile(file, 'utf8', (err, data) => {
-=======
-    fs.readdirSync('./modules').forEach(file => {
-        file = path.join(__dirname, '/modules/' + file);
-        fs.readFile(file, 'utf8', function (err, data) {
->>>>>>> origin/master
             if (err) throw err;
             object = JSON.parse(data);
             Object.keys(object).map((objectKey) => {
@@ -21,11 +15,7 @@ gulp.task('build', () => {
                 array.push(value);
             });
             const json = JSON.stringify(array, null, 4);
-<<<<<<< HEAD
             fs.writeFile("Material-Lite.sublime-theme", json, (err) => {
-=======
-            fs.writeFile("Material-Lite.sublime-theme", json, function(err) {
->>>>>>> origin/master
                 if (err) console.log(err);
                 console.log("The file was saved!");
             });
