f9fcd2b8529f512ec0294e54d68dfded77b1d629
Mauro Vieira <mauro.vieira@namecheap.com>
2018-12-27 12:41:50 +0000

    chore: removed dark assets.

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ function scheme() {
 const build = () => {
     scheme();
     theme("Mariana-Theme-Light", "light");
-    theme("Mariana-Theme-Dark", "dark");
+    // theme("Mariana-Theme-Dark", "dark");
 };
 
 gulp.task("default", build);
