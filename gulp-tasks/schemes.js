294c87c3f4ae3ba7adfd52913d7395eb166acb9d
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-01-23 20:20:13 +0000

    feat: updated eslint.

--- gulp-tasks/schemes.js
+++ gulp-tasks/schemes.js
@@ -1,21 +1,24 @@
-/* eslint-disable semi */
-import path from "path";
-import fs from "fs";
+import path from 'path';
+import fs from 'fs';
 
 function build() {
-    let filePath;
-    fs.readdirSync("./src/schemes/").forEach(file => {
-        filePath = path.join(__dirname, "/../src/schemes/" + file);
-        file = file.split(".").slice(0, -1).join(".");
-        fs.readFile(filePath, "utf8", (err, data) => {
-            if(err) throw err;
-            fs.writeFileSync(`schemes/${file}.sublime-color-scheme`, data, (err) => {
-                if (err) console.log(err);
-            });
-        });
+  let filePath;
+  fs.readdirSync('./src/schemes/').forEach(file => {
+    filePath = path.join(__dirname, '/../src/schemes/' + file);
+    file = file.split('.').slice(0, -1).join('.');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if(err) {
+        throw err;
+      }
+      fs.writeFileSync(`schemes/${file}.sublime-color-scheme`, data, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
     });
+  });
 }
 
 export default function schemes() {
-    build();
+  build();
 }
--- gulp-tasks/themes.js
+++ gulp-tasks/themes.js
@@ -1,31 +1,31 @@
-/*eslint-disable semi*/
-import path from "path";
-import fs from "fs";
-import variables from "./../src/themes/variables.json";
+import path from 'path';
+import fs from 'fs';
+import variables from './../src/themes/variables.json';
 
 function build(name, folder) {
-    const merge = [];
-    let rules = {};
-    fs.readdirSync(`./src/themes/${folder}/`).forEach(file => {
-        file = path.join(__dirname, `/../src/themes/${folder}/` + file);
-        fs.readFile(file, "utf8", (err, data) => {
-            if (err) throw err;
-            rules = JSON.parse(data);
-            Object.keys(rules).map((rulesKey) => {
-                const value = rules[rulesKey];
-                merge.push(value);
-            });
-            const theme = {
-                "variables": variables,
-                "rules": merge
-            };
-            fs.writeFileSync(`${name}.json`, JSON.stringify(theme, null, 4), (err) => {
-                if (err) console.log(err);
-            });
-        });
+  const merge = [];
+  let rules = {};
+  fs.readdirSync(`./src/themes/${folder}/`).forEach(file => {
+    file = path.join(__dirname, `/../src/themes/${folder}/` + file);
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+      rules = JSON.parse(data);
+      Object.keys(rules).map((rulesKey) => {
+        const value = rules[rulesKey];
+        merge.push(value);
+      });
+      const theme = { 'variables': variables, 'rules': merge };
+      fs.writeFileSync(`${name}.json`, JSON.stringify(theme, null, 4), (e) => {
+        if (e) {
+          console.log(e);
+        }
+      });
     });
+  });
 }
 
 export default function themes() {
-    build("Meetio", "default");
+  build('Meetio', 'default');
 }
