09cedc602f95ce57cad0ec29ac8838e087c78c70
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-02-20 16:08:45 +0000

    fix: validate if is a file.

--- gulp-tasks/schemes.js
+++ gulp-tasks/schemes.js
@@ -6,33 +6,32 @@ import rules from './../src/schemes/files/rules.json';
 function build() {
   let filePath;
   fs.readdirSync('./src/schemes/').forEach(file => {
-    if (file === 'files') {
-      return;
-    }
     filePath = path.join(__dirname, '/../src/schemes/' + file);
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-
-      const options = {
-        globals,
-        rules,
-      };
-
-      data = JSON.parse(data);
-      const scheme = Object.assign(data, options);
-
-      fs.writeFileSync(
-        `schemes/${data.name}.sublime-color-scheme`,
-        JSON.stringify(scheme, null, 4),
-        err => {
-          if (err) {
-            console.log(err);
-          }
+    if (fs.lstatSync(filePath).isFile()) {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          throw err;
         }
-      );
-    });
+
+        const options = {
+          globals,
+          rules,
+        };
+
+        data = JSON.parse(data);
+        const scheme = Object.assign(data, options);
+
+        fs.writeFileSync(
+          `schemes/${data.name}.sublime-color-scheme`,
+          JSON.stringify(scheme, null, 4),
+          err => {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      });
+    }
   });
 }
 
