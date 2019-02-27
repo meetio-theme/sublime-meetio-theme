65777ef1f0513a06d275632b73655c5976d16812
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-02-27 23:29:11 +0000

    feat: added py example.

---
+++ examples/file.py
@@ -0,0 +1,18 @@
+class MyClass:
+    "This is my second class"
+    a = 10
+    def func(self):
+        print('Hello')
+
+# create a new MyClass
+ob = MyClass()
+
+# Output: <function MyClass.func at 0x000000000335B0D0>
+print(MyClass.func)
+
+# Output: <bound method MyClass.func of <__main__.MyClass object at 0x000000000332DEF0>>
+print(ob.func)
+
+# Calling function func()
+# Output: Hello
+ob.func()
