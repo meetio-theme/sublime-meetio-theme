c943d4c079e699460af4eb5cca334c1e70af2232
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-06-21 12:11:33 +0100

    feat: add more Python syntax improvements.

--- examples/python.py
+++ examples/python.py
@@ -1,4 +1,4 @@
-class MyClass:
+class MyClass():
     "This is my second class"
     a = 10
     def func(self):
@@ -6,6 +6,42 @@ class MyClass:
 
 # create a new MyClass
 ob = MyClass()
+
+class Model(BaseModel):
+    age: int,
+    first_name = 'John'
+    last_name: NoneStr = None
+    signup_ts: Options[datetime] = None
+    list_of_ints: List[int]
+
+class Addition:
+    first = 0
+    second = 0
+    answer = 0
+
+    # parameterized constructor
+    def __init__(self, f, s):
+        self.first = f
+        self.second = s
+
+    def display(self):
+        print("First number = " + str(self.first))
+        print("Second number = " + str(self.second))
+        print("Addition of two numbers = " + str(self.answer))
+
+    def calculate(self):
+        self.answer = self.first + self.second
+
+# creating object of the class
+# this will invoke parameterized constructor
+obj = Addition(1000, 2000)
+
+# perform Addition
+obj.calculate()
+
+# display result
+obj.display()
+
 
 # Output: <function MyClass.func at 0x000000000335B0D0>
 print(MyClass.func)
