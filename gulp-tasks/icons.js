7424c0d560f2d1180f459a0c106d1ec1f76578dc
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-09-24 11:13:38 +0100

    feat: add new icons.

--- gulp-tasks/icons.js
+++ gulp-tasks/icons.js
@@ -1,11 +1,11 @@
 import path from 'path';
 import fs from 'fs';
-import svg2png from 'svg2png';
+import svg2img from 'svg2img';
 import defaultOptions from './../src/icons/default.json';
 
 function build(folder) {
   let iconPath;
-  fs.readdirSync('./src/icons/svg').forEach(icon => {
+  fs.readdirSync('./src/icons/svg/').forEach(icon => {
     iconPath = path.join(__dirname, `/../src/icons/svg/${icon}`);
     icon = icon
       .split('.')
@@ -18,21 +18,22 @@ function build(folder) {
       data = data.replace('#000', defaultOptions.color);
       data = Buffer.from(data, 'utf8');
       defaultOptions.settings.forEach(setting => {
-        svg2png(data, {width: setting.size, height: setting.size})
-          .then(buffer => {
-            fs.writeFileSync(
-              `assets/${folder}/${
-                setting.suffix ? icon + setting.suffix : icon
-              }.png`,
-              buffer,
-              err => {
-                if (err) {
-                  console.log(err);
-                }
+        svg2img(data, {width: setting.size, height: setting.size}, function(
+          error,
+          buffer
+        ) {
+          fs.writeFileSync(
+            `assets/${folder}/${
+              setting.suffix ? icon + setting.suffix : icon
+            }.png`,
+            buffer,
+            err => {
+              if (err) {
+                console.log(err);
               }
-            );
-          })
-          .catch(e => console.error(e));
+            }
+          );
+        });
       });
     });
   });
