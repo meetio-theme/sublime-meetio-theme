cbe596384e95efdbd0e0af719df24add5610133e
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-02-14 13:01:32 +0000

    fix: scheme generate

--- src/schemes.ts
+++ src/schemes.ts
@@ -2,15 +2,18 @@ import {
     generateScheme,
     ISchemeSetting,
     IColors,
+    IRules
 } from '@meetio/scheme-generator';
 import * as pallete from '@meetio/meetio-colors';
 
+// common rules for all schemes
 import rules from './schemes/index';
 
 interface IScheme {
     name: string;
     author: string;
     variables: IColors;
+    customRules: Array<IRules>; // specific rules for each IScheme
 }
 
 [
@@ -18,19 +21,18 @@ interface IScheme {
         name: 'Meetio-Theme-Dark',
         author: 'Mauro Reis Vieira <mauroreisvieira@gmail.com>',
         variables: pallete.dark,
+        customRules: [],
     },
     {
         name: 'Meetio-Theme-Light',
         author: 'Mauro Reis Vieira <mauroreisvieira@gmail.com>',
         variables: pallete.light,
+        customRules: [],
     },
 ].map((item: IScheme) => {
     const settings: ISchemeSetting = {
         colors: item.variables,
-        rules: [
-            ...rules,
-            ...[],
-        ],
+        rules: [...rules, ...item.customRules],
     };
     generateScheme(item.name, item.author, item.name, settings, 'schemes');
 });
