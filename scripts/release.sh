3a521f03bc58ff3f50ec085f2b49053fbff4cd8c
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-12-15 14:55:54 +0000

    feat: add support for `themed_title_bar`

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,12 +7,12 @@ read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing v$VERSION ..."
+  echo "Releasing 4070-$VERSION ..."
   npm run lint
 
   npm run build
   # generate the version so that the changelog can be generated too
-  npm version --no-git-tag-version --no-commit-hooks --new-version $VERSION
+  npm version --no-git-tag-version --no-commit-hooks --new-version "4070-$VERSION"
 
   # changelog
   npm run changelog
@@ -22,10 +22,10 @@ then
 
   # commit and tag
   git add CHANGELOG.md package.json
-  git commit -m "chore(release): v$VERSION"
-  git tag -a "v$VERSION" -m $VERSION
+  git commit -m "chore(release): 4070-$VERSION"
+  git tag -a "4070-$VERSION" -m "4070-$VERSION"
 
   # publish
-  git push origin refs/tags/v$VERSION
+  git push origin refs/tags/4070-$VERSION
   git push
 fi
