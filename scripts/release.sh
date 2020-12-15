1c53a4d2bc17561ab2a542b132e5c8342d06667f
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2020-12-15 15:07:32 +0000

    chore: update script for release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,31 +1,30 @@
 set -e
-echo "Current version:" $(grep version package.json | sed -E 's/^.*"(4[^"]+)".*$/\1/')
+echo "Current version: "$(grep version package.json | sed -E 's/^.*"(4[^"]+)".*$/\1/')
 echo "Enter release version: "
 read VERSION
 
-read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
+read -p "Releasing 4070-$VERSION - are you sure? (y/n)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Releasing 4070-$VERSION ..."
   npm run lint
 
   npm run build
-  # generate the version so that the changelog can be generated too
-  npm version --no-git-tag-version --no-commit-hooks --new-version "4070-$VERSION"
+  # generate the version so that the CHANGELOG can be generated too
+  npm version --no-git-tag-version --no-commit-hooks --new-version $VERSION
 
-  # changelog
+  # CHANGELOG
   npm run changelog
 
-  echo "Please check the git history and the changelog and press enter"
+  echo "Please check the git history and the CHANGELOG and press enter"
   read OKAY
 
-  # commit and tag
+  # COMMIT AND TAG
   git add CHANGELOG.md package.json
   git commit -m "chore(release): 4070-$VERSION"
   git tag -a "4070-$VERSION" -m "4070-$VERSION"
 
-  # publish
+  # PUBLISH
   git push origin refs/tags/4070-$VERSION
   git push
 fi
