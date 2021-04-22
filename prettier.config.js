8cb1bc267e7d975fbd4859973e16a9b37140c92f
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-04-22 17:35:48 +0100

    fix: remove all

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-/*eslint-disable no-undef */
-module.exports = {
-    trailingComma: "es5",
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-};
--- src/settings/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as lighter } from './lighter';
-export { default as darker } from './darker';
-export { default as palenight } from './palenight';
-export { default as deepocean } from './deepocean';
